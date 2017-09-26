8480dfec5d0937366395e73e3c5f641abdcc7d18
dengyu <1391674791@qq.com>
2017-09-26 10:28:53 +0800

    详情页面初步静态雏形构成

--- src/com/qst/dao/IndexShowDao.java
+++ src/com/qst/dao/IndexShowDao.java
@@ -3,10 +3,9 @@ package com.qst.dao;
 import java.util.List;
 
 import com.qst.model.Goods;
-import com.qst.model.Image;
 import com.qst.model.Page;
 
 public interface IndexShowDao {
-	public List<Goods> indexShow(Page<Image> page);
+	public List<Goods> indexShow(Page<Goods> page);
 	public int findImageNum();
 }
