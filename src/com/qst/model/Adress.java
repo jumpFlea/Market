5c96276a6c2542eb74947be514e370a6ff6469cd
dengyajian <773814380@qq.com>
2017-09-29 17:07:31 +0800

    模型层增加一个类

---
+++ src/com/qst/model/Adress.java
@@ -0,0 +1,50 @@
+package com.qst.model;
+
+
+public class Adress {
+	
+	private String name;
+	private String phone;
+	private String region;
+	private String street;
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String toString() {
+		return "Adress [name=" + name + ", phone=" + phone + ", region=" + region + ", street=" + street + "]";
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+}
