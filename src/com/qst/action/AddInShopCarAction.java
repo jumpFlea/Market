7758bb87c6b955a1012805dd8beac8363b639a4f
dengyajian <773814380@qq.com>
2017-10-01 09:26:36 +0800

    完成加入购物车的action dao service

--- src/com/qst/action/AddInShopCarAction.java
+++ src/com/qst/action/AddInShopCarAction.java
@@ -1,19 +1,35 @@
 package com.qst.action;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.ServletActionContext;
+import org.springframework.stereotype.Component;
 
+import com.qst.serviceImpl.AddIntoCarImpl;
+
+@Component
 public class AddInShopCarAction {
-	
-	public void  addInshopcar(){
-		HttpServletResponse response = ServletActionContext.getResponse();
-		HttpServletRequest request =ServletActionContext.getRequest();
-		HttpSession session =request.getSession();
-		int uid =(int)session.getAttribute("uid");
-		
+	@Resource
+	private AddIntoCarImpl addIntoCarImpl;
+
+	public AddIntoCarImpl getAddIntoCarImpl() {
+		return addIntoCarImpl;
 	}
-	
+
+	public void setAddIntoCarImpl(AddIntoCarImpl addIntoCarImpl) {
+		this.addIntoCarImpl = addIntoCarImpl;
+	}
+
+	public void addInshopcar() {
+		HttpServletResponse response = ServletActionContext.getResponse();
+		HttpServletRequest request = ServletActionContext.getRequest();
+		HttpSession session = request.getSession();
+		int uid = (int) session.getAttribute("uid");
+		int gid = Integer.parseInt(request.getParameter("gid"));
+		addIntoCarImpl.addInshopcar(uid, gid);
+	}
+
 }
--- src/com/qst/action/ShopcarAction.java
+++ src/com/qst/action/ShopcarAction.java
@@ -19,6 +19,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 @Component
 public class ShopcarAction {
 	@Resource
